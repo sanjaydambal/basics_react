bf50c49e1c9c0f3f495b3d62a6d4b0fbbcc34109
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-03-21 21:26:09 +0530

    setState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './Components/Greet';
-import Welcome from './Components/Welcome';
-import Message from './Components/Message';
+// import Greet from './Components/Greet';
+// import Welcome from './Components/Welcome';
+// import Message from './Components/Message';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
       
-      <Greet name = "vikas" lname = "balikai"><p> He works in HCL</p></Greet>
+      {/* <Greet name = "vikas" lname = "balikai"><p> He works in HCL</p></Greet>
       <Greet name = "Rajat" lname = "Acharya"> <p>He is a TPO </p></Greet>
       <Greet name = "vinay" lname = "Bilagi"/>
       <Welcome name = "Lakshmikant" ><strong> He is a solution Architect</strong> </Welcome>
-      <Message/>
+      <Message/> */}
+      <Counter/>
     </div>
   );
 }
