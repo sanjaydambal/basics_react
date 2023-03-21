0943b8de0416d66dee5ff95609dc3f22c1e2c0dd
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-03-21 21:42:22 +0530

    setState with function

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -8,20 +8,33 @@ import React, { Component } from 'react'
        count : 0
     }
   }
-counter() {
-  this.setState({
-    count:this.state.count+1
-  },
-  () =>{
-    console.log(this.state.count)
-  }
-  )
+// counter() {
+//   this.setState({
+//     count:this.state.count+1
+//   },
+//   () =>{
+//     console.log(this.state.count)
+//   }
+//   )
+// }
+increment() {
+  this.setState((prevState)=>({
+    count:prevState.count+1
+  }))
 }
+counterFive(){
+  this.increment()
+  this.increment()
+  this.increment()
+  this.increment()
+  this.increment()
+}
+
   render() {
     return (
       <div>
         <div>Count - {this.state.count}</div>
-        <button onClick={() => this.counter()}>Increment</button>
+        <button onClick={() => this.counterFive()}>Increment</button>
       </div>
     )
   }
