ee078853091c16e29b5fb5007f3dee68b842bace
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-03-22 14:26:51 +0530

    destructuring of props

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-// function Greet(props) {
-//     return (
-//         <div>
-//             <h1>Hello Sanjay</h1>
-//         </div>
-//     );
-// }
-const Greet = (props) => {
-
-return ( 
-    <>
-<h1> Hello {props.name} {props.lname}</h1>
-{props.children}
-</>
-);
+function Greet({name,lname}) {
+    return (
+        <div>
+            <h1>Hello {name} {lname}</h1>
+        </div>
+    );
 }
+// const Greet = (props) => {
+
+// return ( 
+//     <>
+// <h1> Hello {props.name} {props.lname}</h1>
+// {props.children}
+// </>
+// );
+// }
 export default Greet;
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 export default class Welcome extends Component {
   render() {
+    const {name} = this.props
     return (
       <>
-      <h1>Hi {this.props.name}</h1>
-      {this.props.children}
+      <h1>Hi {name}</h1>
+     
       </>
     )
   }
